96a8a75c72f69953a7ab254876784b2eca60e77b
ratherforky <34199683+ratherforky@users.noreply.github.com>
2025-02-06 15:32:45 +0000

    applicative seminar

---
+++ misc/src/FunctorApplicativeMonad.hs
@@ -0,0 +1,65 @@
+{-# language InstanceSigs #-}
+module FunctorApplicativeMonad where
+
+import Prelude hiding (Functor(..), Applicative(..))
+
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+
+instance Functor (Either a) where
+  fmap :: (b -> c) -> Either a b -> Either a c
+  fmap f (Left a) = Left a
+  fmap f (Right x) = Right (f x)
+
+instance Functor Maybe where
+  fmap f Nothing = Nothing
+  fmap f (Just x) = Nothing
+
+-- >>> id Nothing
+-- Nothing
+-- >>> fmap id Nothing
+-- Nothing
+
+-- >>> fmap id (Just 3)
+-- Nothing
+
+(<$>) :: Functor f => (a -> b) -> f a -> f b
+(<$>) = fmap
+
+-- fmap :: (a -> b) -> f a -> f b
+
+class Functor f => Applicative f where
+  liftA2 :: (a -> (b -> c)) -> f a -> f b -> f c
+  -- liftA2 f mx my = (pure f <*> mx) <*> my
+  liftA2 f mx my = (fmap f mx) <*> my
+
+
+  pure :: a -> f a
+  (<*>) :: f (a -> b) -> f a -> f b -- pronounced 'app' or 'tie fighting'
+
+-- fmapp :: Applicative f => (a -> b) -> f a -> f b
+-- fmapp f mx = pure f <*> mx
+
+-- it's like function application!
+-- littleApp :: (a -> b) -> a -> b
+-- littleApp f x = f x
+
+-- f :: a -> (b -> c)
+
+-- >>> zip [1,2,3,4,5] [True, False, True, False]
+-- [(1,True),(2,False),(3,True),(4,False)]
+
+instance Applicative Maybe where
+  -- liftA2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+  -- liftA2 _ Nothing _ = Nothing
+  -- liftA2 _ _ Nothing = Nothing
+  -- liftA2 f (Just x) (Just y) = Just (f x y)
+
+  pure :: a -> Maybe a
+  pure x = Just x
+
+  (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
+  Nothing <*> mb      = Nothing
+  Just f  <*> Nothing = Nothing
+  Just f  <*> Just x  = Just (f x)
